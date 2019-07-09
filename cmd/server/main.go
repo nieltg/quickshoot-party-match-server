e0dde9ca5de4eed4d49d823197ceaf99cc7d48f3
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2019-07-09 12:35:26 +0700

    Adding start message

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
@@ -8,9 +9,13 @@ import (
 )
 
 func main() {
+	fmt.Println("Server is starting...")
+
 	server := new(appHttp.Server)
 	server.DeferredRequestMaxDuration = 30 * time.Second
 	server.Domain.JoinMaxDuration = 5 * time.Minute
+
+	fmt.Println("Server is started!");
 
 	http.ListenAndServe(":8080", server.Handler())
 }
