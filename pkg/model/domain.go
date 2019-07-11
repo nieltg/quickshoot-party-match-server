19201a65ba753ad7aca6cd935ff96bd5ac642406
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2019-07-11 18:33:48 +0700

    Remove MaximumJoinDurationInSeconds()
    I think Godot won't need this.

--- pkg/model/domain.go
+++ pkg/model/domain.go
@@ -6,6 +6,4 @@ type Domain interface {
 	CreateRoom(data RoomPayload) Room
 	// Room finds a room by the room ID or return nil if room is missing.
 	Room(roomID uint64) Room
-	//MaximumJoinDurationInSeconds return maximum duration in seconds
-	MaximumJoinDurationInSeconds() float64
 }
