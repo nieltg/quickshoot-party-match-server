583e315a1b01c0958be36fba5ecd8daa0f6b6a62
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2019-07-11 14:49:06 +0700

    Refactor one-letter variable

--- pkg/iohttp/util.go
+++ pkg/iohttp/util.go
@@ -10,48 +10,48 @@ import (
 	"github.com/nieltg/quickshoot-party-match-server/pkg/model"
 )
 
-func (s *Handler) fetchRoom(w http.ResponseWriter, roomIDStr string) model.Room {
+func (s *Handler) fetchRoom(writer http.ResponseWriter, roomIDStr string) model.Room {
 	roomID, err := strconv.ParseUint(roomIDStr, 10, 64)
 	if err != nil {
 		log.Println("Unable to parse room ID:", err)
-		w.WriteHeader(500)
+		writer.WriteHeader(500)
 		return nil
 	}
 
 	room := s.Domain.Room(roomID)
 	if room == nil {
-		w.WriteHeader(404)
+		writer.WriteHeader(404)
 		return nil
 	}
 
 	return room
 }
 
-func decodeJSONBody(w http.ResponseWriter, body io.ReadCloser, v interface{}) bool {
+func decodeJSONBody(writer http.ResponseWriter, body io.ReadCloser, v interface{}) bool {
 	decoder := json.NewDecoder(body)
 
 	if err := decoder.Decode(v); err != nil {
 		log.Println("Unable to decode body:", err)
-		w.WriteHeader(500)
+		writer.WriteHeader(500)
 		return false
 	}
 
 	return true
 }
 
-func writeJSON(w http.ResponseWriter, v interface{}) bool {
+func writeJSON(writer http.ResponseWriter, v interface{}) bool {
 	data, err := json.Marshal(v)
 	if err != nil {
 		log.Println("Unable to marshal JSON output:", err)
-		w.WriteHeader(500)
+		writer.WriteHeader(500)
 		return false
 	}
 
-	w.Header().Set("Content-Type", "application/json")
+	writer.Header().Set("Content-Type", "application/json")
 
-	if _, err = w.Write(data); err != nil {
+	if _, err = writer.Write(data); err != nil {
 		log.Println("Unable to write response:", err)
-		w.WriteHeader(500)
+		writer.WriteHeader(500)
 		return false
 	}
 
