d524cd3e7251ede720902dbf7dd296dcf679bdaa
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2019-07-15 13:12:21 +0700

    TODO: Add new feature to auto-tap when the room auto-deletes

--- pkg/modelmemory/domain.go
+++ pkg/modelmemory/domain.go
@@ -1,9 +1,12 @@
 package modelmemory
 
 import (
+	"fmt"
+	"strconv"
 	"sync"
 	"sync/atomic"
 	"time"
+	"math"
 
 	"github.com/nieltg/quickshoot-party-match-server/pkg/model"
 )
@@ -30,6 +33,28 @@ func (domain *Domain) autoDeleteRoom(room *room) {
 	select {
 	case <-room.deleteChannel:
 	case <-time.After(domain.JoinMaxDuration):
+		//TODO: create correct mechanism for auto-tap at the end of game
+		gameBegins := len(room.tapTimes) > 0
+		if gameBegins {
+			membersClone := room.members
+			for id := range room.tapTimes {
+				membersClone.Delete(id)
+			}
+			//TODO: I believe this process blocks query for time
+			membersClone.Range(func(key, value interface{}) bool {
+				id, _ := strconv.ParseUint(fmt.Sprintf("%d", key), 10, 64)
+				recordStatus := room.RecordTapTime(id, model.MemberTapTimePayload{
+					TimeInMilis: uint64(5.0 * math.Round(time.Minute.Seconds() * 1000.0)),
+				})
+
+				return recordStatus
+			})
+		}
+
+		select {
+		case <-time.After(10 * time.Second):
+			fmt.Println(room.findWinner())
+		}
 	}
 
 	domain.rooms.Delete(room.id)
